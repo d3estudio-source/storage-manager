a1baf62b630e959f7c43a9388d91d87fcdcca406
Jonathan Querubina <jotadev@me.com>
2017-05-16 17:00:30 -0300

    Update emprestimo select

--- storage_controller/inventario/admin.py
+++ storage_controller/inventario/admin.py
@@ -41,7 +41,7 @@ class EmprestimoAdmin(admin.ModelAdmin):
 	list_filter = ('responsavel','status','data_saida', 'data_entrada',)
 
 	def itens(self, obj):
-		return "\n".join([p.item for p in obj.item.all()])
+		return "\n<br>".join([p for p in obj.item.all()])
 
 	def get_form(self, request, obj=None, **kwargs):
 		form = super(EmprestimoAdmin, self).get_form(request, obj, **kwargs)
